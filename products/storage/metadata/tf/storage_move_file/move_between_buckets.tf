a3f64ad93af9fe3e04d007bd32f44584322c6804
Maciej Strzelczyk <strzelczyk@google.com>
2023-12-08 16:45:09 +0100

    feat: Adding UST metadata for tests using Terraform

---
+++ products/storage/metadata/tf/storage_move_file/move_between_buckets.tf
@@ -0,0 +1,41 @@
+variable "bucket_name" {
+    type        = string
+}
+variable "new_bucket_name" {
+    type        = string
+}
+variable "file_name" {
+    type        = string
+}
+
+resource "google_storage_bucket" "default" {
+  name          = var.bucket_name
+  location      = "EU"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "default2" {
+  name          = var.new_bucket_name
+  location      = "EU"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "default" {
+  name   = var.file_name
+  content = " this is the content! "
+  bucket = google_storage_bucket.default.name
+}
+
+
+# export TF_VAR_bucket_name = some-random-name
+# export TF_VAR_new_bucket_name = some-random-name2
+# export TF_VAR_file_name = another-random-name
+# terraform apply
+# or
+# terraform apply -var="bucket_name=unique-bucket-3135sd" -var="new_bucket_name=unique-bucket-3132sd" -var="file_name=test-file"
